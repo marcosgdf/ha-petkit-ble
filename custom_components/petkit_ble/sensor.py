80d467546ff4a1eaa0fb03b459cb56f23b94f21c
Marcos García <marcosgdf@gmail.com>
2026-07-08 19:46:13 +0200

    Added missing dependency in sensor.py

--- custom_components/petkit_ble/sensor.py
+++ custom_components/petkit_ble/sensor.py
@@ -7,7 +7,7 @@ from homeassistant.components.sensor import (
     SensorStateClass,
 )
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import PERCENTAGE, UnitOfEnergy, UnitOfTime, UnitOfVolume
+from homeassistant.const import PERCENTAGE, UnitOfEnergy, UnitOfTime, UnitOfVolume, EntityCategory
 from homeassistant.core import HomeAssistant
 from datetime import datetime
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
